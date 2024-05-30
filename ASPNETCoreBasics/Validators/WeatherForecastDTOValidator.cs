b95d3d4bcee21482ebed980ccbca27008a9bc3bc
JordiTortosa <jordi.tortosa@devoteam.com>
2024-05-30 17:00:40 +0200

    Arreglo fecha mínima

--- ASPNETCoreBasics/Validators/WeatherForecastDTOValidator.cs
+++ ASPNETCoreBasics/Validators/WeatherForecastDTOValidator.cs
@@ -8,7 +8,7 @@
         public WeatherForecastDTORequestValidator()
         {
             RuleFor(x => x.Date).NotEmpty().WithMessage("Date es obligatorio");
-            RuleFor(x => x.Date).NotEmpty().WithMessage("Date No Válido").Must(date => date != new DateOnly(1000, 10, 10));
+            RuleFor(x => x.Date).NotEmpty().WithMessage("Date No Válido").Must(date => date >= new DateOnly(1970, 01, 01));
             RuleFor(x => x.TemperatureC).NotEmpty().WithMessage("TemperatureC es obligatorio");
             RuleFor(x => x.TemperatureC).NotEmpty().WithMessage("TemperatureC No Válido").Must(temperature => temperature != 1000);
             RuleFor(x => x.Summary).NotEmpty().WithMessage("Summary es obligatorio");
